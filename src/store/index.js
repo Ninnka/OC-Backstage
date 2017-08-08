191ce00e90f99f5769324ff3048bd299aa1277cb
zhouguang <zhouguang@sofyun.com>
2017-08-08 11:06:51 +0800

    完善出金弹窗以及vuex增加个人银行列表

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import actions from './actions';
 import mutations from './mutations';
+import CommonApi from '@assets/js/common';
 Vue.use(Vuex);
 
 const debug = process.env.NODE_ENV !== 'production';
@@ -10,10 +11,38 @@ export default new Vuex.Store({
   state: {
     user: {
       name: 'name'
-    }
+    },
+    bankList: [
+      {
+        name: '张三',
+        id: 1,
+        card: '622568032300016316',
+        bankCode: '0001'
+      },
+      {
+        name: '李四',
+        id: 2,
+        card: '622568032300026316',
+        bankCode: '0002'
+      }
+    ]
   },
   getters: {
-    user: state => state.user
+    user: state => state.user,
+    bankList: (state) => {
+      if (state.bankList.length > 0) {
+        return state.bankList.map((item) => {
+          CommonApi.bankList.map((bank) => {
+            if (item.bankCode === bank.bankCode) {
+              item.bankMes = bank;
+            }
+          });
+          return item;
+        });
+      } else {
+        return state.bankList;
+      }
+    }
   },
   actions,
   mutations,
