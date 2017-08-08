c8e83a5b0c43b6028a5cc363429b56b0f5d4e484
zhouguang <zhouguang@sofyun.com>
2017-08-08 16:13:42 +0800

    完善用户管理页面

---
+++ src/pages/user-manage/user-manage-data-mixin.js
@@ -0,0 +1,53 @@
+export default {
+  data () {
+    return {
+      userList: [
+        {
+          account: 'JY00000001',
+          nickName: '陈陈',
+          realName: '陈晨',
+          cardId: '19950812',
+          phone: '13388889999',
+          mtLength: 2,
+          mainMt: '123',
+          accountStatus: true,
+          mentionStatus: false,
+          tradeStatus: false,
+          promotStatus: true,
+          superior: '11',
+          openTime: '2017-01-01 10:00:00'
+        },
+        {
+          account: 'JY00000002',
+          nickName: '驱蚊器',
+          realName: '陈2',
+          cardId: '19950812',
+          phone: '133888899100',
+          mtLength: 2,
+          mainMt: '123',
+          accountStatus: true,
+          mentionStatus: false,
+          tradeStatus: false,
+          promotStatus: true,
+          superior: '112',
+          openTime: '2017-01-01 10:00:00'
+        },
+        {
+          account: 'JY00000001',
+          nickName: '通天塔',
+          realName: '陈3',
+          cardId: '19950812',
+          phone: '13388889999',
+          mtLength: 2,
+          mainMt: '123',
+          accountStatus: true,
+          mentionStatus: false,
+          tradeStatus: false,
+          promotStatus: true,
+          superior: '11',
+          openTime: '2017-01-01 10:00:00'
+        }
+      ]
+    };
+  }
+};
